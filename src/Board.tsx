99cc8a9365863f4177f2b3becf02ede017a560c9
Daniel Berndt <daniel.berndt@gmail.com>
2022-01-16 22:18:53 +0100

    move Board Component into separate file

---
+++ src/Board.tsx
@@ -0,0 +1,89 @@
+import {Col} from "./Box";
+import {AnnotadedLetter} from "./types";
+import LetterBox, {LetterRow} from "./WordBox";
+
+const MAX_GUESSES = 6;
+
+const evaluateWord = ({word, guessWord}: {word: string; guessWord: string}): AnnotadedLetter[] => {
+  const remainingLetters = Array.from(guessWord);
+  const letters = Array.from(word);
+  const correctIdx = new Set<number>();
+  letters.forEach((l, idx) => {
+    if (l === guessWord[idx]) {
+      correctIdx.add(idx);
+      remainingLetters.splice(remainingLetters.indexOf(l), 1);
+    }
+  });
+
+  return letters.map((letter, idx) => {
+    const getType = () => {
+      if (correctIdx.has(idx)) return "correctPosition";
+      if (remainingLetters.includes(letter)) {
+        remainingLetters.splice(remainingLetters.indexOf(letter), 1);
+        return "found";
+      }
+      return "notFound";
+    };
+    return {letter, type: getType()};
+  });
+};
+
+type SubmittedWordProps = {word: string; guessWord: string; isLast: boolean};
+const SubmittedWord = ({word, guessWord, isLast}: SubmittedWordProps) => {
+  const annotatedLetters = evaluateWord({word, guessWord});
+
+  return (
+    <LetterRow>
+      {annotatedLetters.map(({letter, type}, idx) => (
+        <LetterBox key={idx} letter={letter} type={type} animateReveal={isLast} idx={idx} />
+      ))}
+    </LetterRow>
+  );
+};
+
+type BoardProps = {
+  input: string;
+  submittedWords: string[];
+  guessWord: string;
+};
+
+const fillWith = (input: string, fillChar: string, len: number) => {
+  if (input.length >= len) return Array.from(input);
+  return [...input, ...Array.from(new Array(len - input.length), () => fillChar)];
+};
+
+const EmptyRow = () => (
+  <LetterRow>
+    <LetterBox />
+    <LetterBox />
+    <LetterBox />
+    <LetterBox />
+    <LetterBox />
+  </LetterRow>
+);
+
+const Board = ({input, submittedWords, guessWord}: BoardProps) => (
+  <Col sp={1} fillParent px={5} py={5} justify="center">
+    {submittedWords.map((word, idx) => (
+      <SubmittedWord
+        key={idx}
+        word={word}
+        guessWord={guessWord}
+        isLast={idx === submittedWords.length - 1}
+      />
+    ))}
+
+    {submittedWords.length < MAX_GUESSES && (
+      <LetterRow>
+        {fillWith(input, "", 5).map((letter, idx) => (
+          <LetterBox key={idx} letter={letter} />
+        ))}
+      </LetterRow>
+    )}
+    {Array.from(new Array(Math.max(0, MAX_GUESSES - submittedWords.length - 1))).map((_, idx) => (
+      <EmptyRow key={idx} />
+    ))}
+  </Col>
+);
+
+export default Board;
--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import {useState} from "preact/hooks";
+import Board from "./Board";
 import {Box, Col} from "./Box";
 import Frame from "./Frame";
 import Keyboard from "./Keyboard";
@@ -6,7 +7,6 @@ import {BaseButton} from "./LetterButton";
 import {AnnotadedLetter, AnnotatedKeys} from "./types";
 import {themeBright} from "./ui.css";
 import deWords from "./word-lists/valid_words_de.json";
-import LetterBox, {LetterRow} from "./WordBox";
 
 const Heading = () => <div>Heading</div>;
 
@@ -26,88 +26,6 @@ const createGuessWordMapping = (guessWord: string): GuessWordMapping => {
   });
   return mapping;
 };
-
-const evaluateWord = ({word, guessWord}: {word: string; guessWord: string}): AnnotadedLetter[] => {
-  const remainingLetters = Array.from(guessWord);
-  const letters = Array.from(word);
-  const correctIdx = new Set<number>();
-  letters.forEach((l, idx) => {
-    if (l === guessWord[idx]) {
-      correctIdx.add(idx);
-      remainingLetters.splice(remainingLetters.indexOf(l), 1);
-    }
-  });
-
-  return letters.map((letter, idx) => {
-    const getType = () => {
-      if (correctIdx.has(idx)) return "correctPosition";
-      if (remainingLetters.includes(letter)) {
-        remainingLetters.splice(remainingLetters.indexOf(letter), 1);
-        return "found";
-      }
-      return "notFound";
-    };
-    return {letter, type: getType()};
-  });
-};
-
-type SubmittedWordProps = {word: string; guessWord: string; isLast: boolean};
-const SubmittedWord = ({word, guessWord, isLast}: SubmittedWordProps) => {
-  const annotatedLetters = evaluateWord({word, guessWord});
-
-  return (
-    <LetterRow>
-      {annotatedLetters.map(({letter, type}, idx) => (
-        <LetterBox key={idx} letter={letter} type={type} animateReveal={isLast} idx={idx} />
-      ))}
-    </LetterRow>
-  );
-};
-
-type BoardProps = {
-  input: string;
-  submittedWords: string[];
-  guessWord: string;
-};
-
-const fillWith = (input: string, fillChar: string, len: number) => {
-  if (input.length >= len) return Array.from(input);
-  return [...input, ...Array.from(new Array(len - input.length), () => fillChar)];
-};
-
-const EmptyRow = () => (
-  <LetterRow>
-    <LetterBox />
-    <LetterBox />
-    <LetterBox />
-    <LetterBox />
-    <LetterBox />
-  </LetterRow>
-);
-
-const Board = ({input, submittedWords, guessWord}: BoardProps) => (
-  <Col sp={1} fillParent px={5} py={5} justify="center">
-    {submittedWords.map((word, idx) => (
-      <SubmittedWord
-        key={idx}
-        word={word}
-        guessWord={guessWord}
-        isLast={idx === submittedWords.length - 1}
-      />
-    ))}
-
-    {submittedWords.length < MAX_GUESSES && (
-      <LetterRow>
-        {fillWith(input, "", 5).map((letter, idx) => (
-          <LetterBox key={idx} letter={letter} />
-        ))}
-      </LetterRow>
-    )}
-    {Array.from(new Array(Math.max(0, MAX_GUESSES - submittedWords.length - 1))).map((_, idx) => (
-      <EmptyRow key={idx} />
-    ))}
-  </Col>
-);
 
 const WonMessage = ({onReplay}: {onReplay: () => void}) => (
   <Col sp={4} align="center" justify="center" px={4} fillParent pb={4}>
