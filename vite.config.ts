4c874152f037a288c55f9cfd6fdbf991df636cae
Daniel Berndt <daniel.berndt@gmail.com>
2022-01-22 21:15:13 +0100

    add pwa functionality

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,32 @@ import {resolve} from "path";
 import preact from "@preact/preset-vite";
 import {vanillaExtractPlugin} from "@vanilla-extract/vite-plugin";
 import svgr from "vite-plugin-svgr";
+import {VitePWA} from "vite-plugin-pwa";
+import {colors} from "./src/ui/colors";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact(), vanillaExtractPlugin(), svgr()],
+  plugins: [
+    preact(),
+    vanillaExtractPlugin(),
+    svgr(),
+    VitePWA({
+      includeAssets: ["favicon.svg", "robots.txt"],
+      manifest: {
+        name: "Wortle",
+        short_name: "Wortle",
+        description: "Wortle auf Deutsch",
+        theme_color: colors.amber400,
+        icons: [
+          {
+            src: "favicon.svg",
+            sizes: "192x192",
+            type: "image/svg",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       react: "preact/compat",
