0b6dd02136db857cf7ae2c08958d0becce7e1d29
Daniel Berndt <daniel.berndt@gmail.com>
2022-02-06 17:02:06 +0100

    set up cloudflare workers

---
+++ workers/index.ts
@@ -0,0 +1,51 @@
+import {Router} from "itty-router";
+
+const router = Router();
+router.get("/", () => {
+  return new Response("Hello, world! This is the root page of your Worker template.");
+});
+
+router.get<Request>("/example/:text", ({params}) => {
+  // Decode text like "Hello%20world" into "Hello world"
+  let input = decodeURIComponent(params!.text);
+
+  // Construct a buffer from our input
+  let buffer = Buffer.from(input, "utf8");
+
+  // Serialise the buffer into a base64 string
+  let base64 = buffer.toString("base64");
+
+  // Return the HTML with the string to the client
+  return new Response(`<p>Base64 encoding: <code>${base64}</code></p>`, {
+    headers: {
+      "Content-Type": "text/html",
+    },
+  });
+});
+
+router.post<Request>("/post", async (request) => {
+  if (request.headers.get("Content-Type") !== "application/json") {
+    return new Response("Only support content type 'application/json'", {status: 400});
+  }
+  // Create a base object with some fields.
+  let fields = {
+    asn: request.cf?.asn,
+    colo: request.cf?.colo,
+    json: await request.json(),
+  };
+
+  // Serialise the JSON to a string.
+  const returnData = JSON.stringify(fields, null, 2);
+
+  return new Response(returnData, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+});
+
+router.all("*", () => new Response("404, not found!", {status: 404}));
+
+addEventListener("fetch", (e) => {
+  e.respondWith(router.handle(e.request));
+});
